1e6adab5b2be6a70458ffda59cc32bcf426f996d
ashea2120 <ashea2120@yahoo.com>
2019-05-15 09:19:58 -0400

    Initial commit

---
+++ app/src/main/java/com/s/liza/tourguideappodessa/MainActivity.java
@@ -0,0 +1,35 @@
+package com.s.liza.tourguideappodessa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setCustomOnClickListener(R.id.hotel, HotelActivity.class);
+        setCustomOnClickListener(R.id.cafes, CafesActivity.class);
+        setCustomOnClickListener(R.id.fam_attract, TopAttractionsActivity.class);
+        setCustomOnClickListener(R.id.museums, BreweriesActivity.class);
+        setCustomOnClickListener(R.id.catacombs, FestivalsActivity.class);
+    }
+
+    public void setCustomOnClickListener(int resourceID, final Class className) {
+        View view = findViewById(resourceID);
+        view.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(view.getContext(), className);
+                        startActivity(intent);
+                }
+                }
+        );
+    }
+}
